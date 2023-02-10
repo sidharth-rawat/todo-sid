81ec4832b71eb9f6d02889d9ee9369f7200d222b
Sidharth Rawat <73483263+sidharth-rawat@users.noreply.github.com>
2023-02-10 13:32:44 +0530

    fixed some issue

--- app/Controller/Controller.js
+++ app/Controller/Controller.js
@@ -1,109 +1,62 @@
 const http_formatter = require('../../_util/Formtter')
 const Schema = require('../model/todo')
+const { StatusCodes } = require('http-status-codes');
+
 const  getTodo = async(request,response)=>{
-
+    try {
+        const {pageNo, perPage} = request.query;
+        const users = await Schema.find({isDeleted: false}).skip(perPage * (pageNo - 1)).limit(perPage);
     
-    // const {pageNo = 1, perPage = 20} = request.query; // can be undefined in the query params.
-    // const total_count = await Product.find({isDeleted: false}).count();
-    // const has_more = total_count > pageNo * perPage;
-    // const product = await Schema.find({isDeleted: false}).skip(perPage * (pageNo - 1)).limit(perPage);
-    const {pageNo, perPage} = request.query;
-    const users = await Schema.find({}).skip(perPage * (pageNo - 1)).limit(perPage);
+        return response.status(StatusCodes.OK).json(http_formatter(users, "User got successfully"));
+        
+} catch (error) {
+    return response.status(StatusCodes.BAD_REQUEST).json(http_formatter({}, "somethig went wrong in get",false));
+    
+}
 
-    return response.status(200).json(http_formatter(users, "User got successfully"));
-
-
-    // const page = req.query.page || 1 ;
-    // const limit = req.query.limit ||  3;
-    // const skip = (page-1)*limit;
-
-
-    // Todo.find()
-    // .skip(parseInt(skip))
-    // .limit((parseInt(limit)))
-    // .exec((err,todos)=>{
-      
-
-    //      if (err) {
-    //     return res.json({error:err});
-
-    //      }
-    //      return res.json({data:todos});
-    // });
 }
  const createTodo= async (request,response)=>{
     try {
-        const product = await Schema.create(request.body);
-        return response.status(201).json(
-            http_formatter(product, "User created successfully"),
+        const users = await Schema.create(request.body);
+        return response.status(StatusCodes.CREATED).json(
+            http_formatter(users, "User created successfully")
         );
         
-    } catch (err) {
-        return response.status(400).json(http_formatter(err, "Something went wrong, please try again", false));
+    } catch (error) {
+        return response.status(StatusCodes.BAD_REQUEST).json(http_formatter(error, "Something went wrong, please try again", false));
    
-    }
-    // const todo = Todo({
-    //     title:req.body.title,
-    //     content: req.body.content,
-    // });
-    // todo.save((err,todo)=>{
-    //     if (err) {
-    //         return res.json({error:err});
-    
-    //     }
-    //     return res.json({data:todo});
-    // }); 
+    } 
  }
 const updateTodo = async  (request,response)=>{
-try {
-    
-    const {todo_id} = request.params;
-        if(!todo_id) {
-            throw (new Error('Product ID is mandatory'));
+    try {
+    const { title, content }  =  request.body;
+    const query ={_id : request.params.id}
+            if(!query) {
+            throw (new Error('users ID is mandatory'));
         }
-        const current_product = await Product.findById(product_id);
-        if(!current_product) {
-            return response.status(400).json(http_formatter({}, "Invalid user id", false));
+    const users = await Schema.findByIdAndUpdate(
+        query,{
+            title,
+            content
         }
+    );
+        return response.status(StatusCodes.OK).json(http_formatter(users, "Scuccse"));
+               
         
 } catch (error) {
-    
-}
-    // Todo.findById(req.params.id).exec((err,todo)=>{
-    //     if (err) {
-    //         return res.json({error:err});
-    //  }
-    //  todo.title = req.body.title ?? todo.title;
-    //  todo.content = req.body.content ?? todo.content;
-    //  todo.completed = req.body.completed ?? todo.completed;
-    //  todo.save((err, todo)=>{
-    //     if (err) {
-    //         return res.json({error:err});
-    //  }return res.json({data:todo});
-    //  })
-    // })
-    
+    return response.status(StatusCodes.BAD_REQUEST).json(http_formatter(error, "SOmething went wrong in update", false));
+        
+}   
 }
 const deleteTodo = async (request,response)=>{
 try {
     const {todo_id} = request.params;
-    const deleted = await Cart.findOneAndUpdate(todo_id, {isDeleted: true});
-    return response.status(200).json(http_formatter(deleted, "Card deleted successfully", true));
+    const deleted = await Schema.deleteOne(todo_id, {isDeleted: true});
+    return response.status(StatusCodes.OK).json(http_formatter(deleted, "Card deleted successfully"));
     
 } catch (error) {
-    
+    return response.status(StatusCodes.BAD_GATEWAY).json(http_formatter(error, "SOmething went wrong in delete", false));
+
 }
-
-    // Todo.remove({
-    //     _id : req.params.id
-    // }).exec((err,result)=>{
-    //     if (err) {
-    //         return res.json({error:err});
-    //  }if (result.deleteCount == 0) {
-    //     return res.json({data:"No todo found in id"});
-    //  }
-    //     return res.json({data:"deleted successfully"});
-    // })
-
 }
 module.exports = {getTodo ,createTodo ,deleteTodo,updateTodo}
--- index.js
+++ index.js
@@ -2,7 +2,8 @@ const express = require('express');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const todoRoutes = require('./app/routes/todo');
-
+const http_formatter = require('./_util/Formtter');
+const { StatusCodes } = require('http-status-codes');
 const app = express();
 dotenv.config();
 const PORT = process.env.PORT;
@@ -15,13 +16,13 @@ app.use('/todo', todoRoutes);
 
 app.use((req, res, next) => {
   const error = new Error('Invalid request');
-  res.status = 404; // not found.
+  res.status = StatusCodes.NOT_FOUND; // not found.
   next(error);
 });
 
 // this is the last route that will get hit, if there's no matching route or some error has occurred.
 app.use((error, req, res, next) => {
-  res.status = error.status || 500;
+  res.status = error.status || StatusCodes.INTERNAL_SERVER_ERROR;
   return res.json(
       http_formatter(error, error.message, false)
   );
